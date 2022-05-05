75ed163c2b16900ff638d89dc7058cddf9f0385a
Kanhaiya Suthar <kanhaiyasuthar0@gmail.com>
2022-05-05 17:36:57 +0530

    ma

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./src/controllers/user");
 const connect = require("./src/config/db");
-const PORT =  9008;
+const PORT = process.env.PORT ||  9008;
 
 http.createServer(app).listen(PORT, async ()=>{
    await connect();
