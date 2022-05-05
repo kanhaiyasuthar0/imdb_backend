750df3e9629edfe438b930a695cdc7a5957cf2d4
Kanhaiya Suthar <kanhaiyasuthar0@gmail.com>
2022-05-05 23:56:15 +0530

    ma

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,9 +3,9 @@ const app = express();
 const passport = require("../../auth/google");
 const user = require("../models/user");
 app.use(passport.initialize());
-app.get("/", (req,res,next)=>{
-    res.send("Your app is ready")
-})
+app.get("/", (req, res, next) => {
+  res.send("Your app is ready");
+});
 app.get("/signin", (req, res, next) => {
   res.send();
 });
@@ -14,8 +14,8 @@ app.get("/failed", (req, res) => {
   res.send("Failed");
 });
 app.get("/success", (req, res) => {
-    // let time = new Date();
-    // console.log(time);
+  // let time = new Date();
+  // console.log(time);
 
   res.send(`Successfully login`);
 });
@@ -32,31 +32,23 @@ app.get(
   passport.authenticate("google", {
     failureRedirect: "/failed",
   }),
- async function (req, res) {
-     let email = (req.user.emails[0].value);
-    
-    let response =await user.findOne({email}).lean().exec();
-    if(response){
+  async function (req, res) {
+    let email = req.user.emails[0].value;
 
-       console.log("hello")
-    }else{
-       
-    let userObj = {
-        email:req.user.emails[0].value ,
+    let response = await user.findOne({ email }).lean().exec();
+    if (response) {
+    } else {
+      let userObj = {
+        email: req.user.emails[0].value,
         password: "NA",
         firstName: req.user._json.given_name,
-        lastName:req.user._json.family_name,
+        lastName: req.user._json.family_name,
       };
 
-      let response =  await user.insertMany([userObj]);
-      
+      let response = await user.insertMany([userObj]);
     }
-    
+
     res.redirect("/success");
-
-
-
-    
 
     // console.log(response)
   }
