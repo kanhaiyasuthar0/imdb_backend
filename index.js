737b9590f4b7909c4aa82524434322f7a58250c7
Kanhaiya Suthar <kanhaiyasuthar0@gmail.com>
2022-05-06 07:47:20 +0530

    ma

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const connect = require("./src/config/db");
 const PORT = process.env.PORT ||  9007;
 console.log(PORT)
 http.createServer(app).listen(PORT, async ()=>{
-   await connect();
+//    await connect();
     console.log(`Server is connected ${PORT}`)
 })
